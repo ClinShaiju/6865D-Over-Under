219c6489794c8b7df7f483bec34f849505c4fa5b
Clin Shaiju <63211630+ClinShaiju@users.noreply.github.com>
2023-11-18 12:10:25 -0600

    Change to using 1 IMU instead of average of 2

--- src/subsystems/drivetrain.cpp
+++ src/subsystems/drivetrain.cpp
@@ -42,8 +42,8 @@ lemlib::TrackingWheel yTracker(&yEncoder, 2.75, -0.0196850394, 1);
 
 pros::Imu inertial1 = pros::Imu(PORT_INERTIAL_1);
 pros::Imu inertial2 = pros::Imu(PORT_INERTIAL_2);
-std::vector<pros::Imu*> imuList{&inertial1, &inertial2};
-std::vector<float> errorMultiplierList{1, 1};
+std::vector<pros::Imu*> imuList{&inertial2};
+std::vector<float> errorMultiplierList{1};
 lemlib::ImuGroup imuGroup = lemlib::ImuGroup(imuList, errorMultiplierList);
 
 
