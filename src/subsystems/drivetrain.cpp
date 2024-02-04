52fe986aed73082fd31f7ffa4c50ca6f53442d03
Clin Shaiju <63211630+ClinShaiju@users.noreply.github.com>
2024-02-04 15:37:48 -0600

    Reverse drivetrain motors for new drivetrain

--- src/subsystems/drivetrain.cpp
+++ src/subsystems/drivetrain.cpp
@@ -11,10 +11,10 @@ pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 pros::Motor backLeft = pros::Motor(PORT_DRIVE_BACKLEFT, pros::E_MOTOR_GEARSET_06, true);
 pros::Motor frontLeft = pros::Motor(PORT_DRIVE_FRONTLEFT, pros::E_MOTOR_GEARSET_06, true);
-pros::Motor middleLeft = pros::Motor(PORT_DRIVE_MIDDLELEFT, pros::E_MOTOR_GEARSET_06, true);
+pros::Motor middleLeft = pros::Motor(PORT_DRIVE_MIDDLELEFT, pros::E_MOTOR_GEARSET_06, false);
 pros::Motor frontRight = pros::Motor(PORT_DRIVE_FRONTRIGHT, pros::E_MOTOR_GEARSET_06, false);
 pros::Motor backRight = pros::Motor(PORT_DRIVE_BACKRIGHT, pros::E_MOTOR_GEARSET_06, false);
-pros::Motor middleRight = pros::Motor(PORT_DRIVE_MIDDLERIGHT, pros::E_MOTOR_GEARSET_06, false);
+pros::Motor middleRight = pros::Motor(PORT_DRIVE_MIDDLERIGHT, pros::E_MOTOR_GEARSET_06, true);
 
 pros::Motor_Group driveLeft = pros::Motor_Group({backLeft, frontLeft, middleLeft});
 pros::Motor_Group driveRight = pros::Motor_Group({backRight, frontRight, middleRight});
@@ -23,8 +23,8 @@ lemlib::Drivetrain_t drivetrain {
         &driveLeft, // left drivetrain motors
         &driveRight, // right drivetrain motors
         12.5590551, // track width
-        4.125, // wheel diameter
-        257.142857, // wheel rpm
+        4, // wheel diameter
+        300, // wheel rpm
         4000 // chase power
 };
 
@@ -34,11 +34,11 @@ lemlib::Drivetrain_t drivetrain {
 ##############
 */
 
-pros::ADIEncoder xEncoder = pros::ADIEncoder(PORT_ADI_XENCODER_TOP, PORT_ADI_XENCODER_BOTTOM, true);
-pros::ADIEncoder yEncoder = pros::ADIEncoder(PORT_ADI_YENCODER_TOP, PORT_ADI_YENCODER_BOTTOM, false);
+//pros::ADIEncoder xEncoder = pros::ADIEncoder(PORT_ADI_XENCODER_TOP, PORT_ADI_XENCODER_BOTTOM, true);
+//pros::ADIEncoder yEncoder = pros::ADIEncoder(PORT_ADI_YENCODER_TOP, PORT_ADI_YENCODER_BOTTOM, false);
 
-lemlib::TrackingWheel xTracker(&xEncoder, 2.75, -1.00393701, 1);
-lemlib::TrackingWheel yTracker(&yEncoder, 2.75, -0.0196850394, 1);
+//lemlib::TrackingWheel xTracker(&xEncoder, 2.75, -1.00393701, 1);
+//lemlib::TrackingWheel yTracker(&yEncoder, 2.75, -0.0196850394, 1);
 
 pros::Imu inertial1 = pros::Imu(PORT_INERTIAL_1);
 pros::Imu inertial2 = pros::Imu(PORT_INERTIAL_2);
@@ -52,7 +52,7 @@ lemlib::OdomSensors_t odomSensors {
         nullptr, // vertical tracking wheel 2
         nullptr,//&xTracker, // horizontal tracking wheel 1
         nullptr, // we don't have a second tracking wheel, so we set it to nullptr
-        &imuGroup
+        nullptr
 };
 
 /*
