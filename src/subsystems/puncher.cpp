7b05d814934f267c2f495c2a1d0a387bd8bd9703
Clin Shaiju <63211630+ClinShaiju@users.noreply.github.com>
2024-02-04 15:33:05 -0600

    Update slapper for new bot rebuild and use helper functions to run motor

--- src/subsystems/puncher.cpp
+++ src/subsystems/puncher.cpp
@@ -1,6 +1,6 @@
 #include "main.h"
 
-pros::Motor puncher = pros::Motor(PORT_PUNCHER, pros::E_MOTOR_GEARSET_06, true);
+pros::Motor puncher = pros::Motor(PORT_PUNCHER, pros::E_MOTOR_GEARSET_06, false);
 bool puncherPressed = false;
 bool puncherRunning = false;
 
@@ -8,11 +8,11 @@ void runPuncherToggle() {
     if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
         if (!puncherPressed) {
             if (!puncherRunning) {
-                puncher = 127;
+                startPuncher();
                 puncherRunning = true;
             }
             else {
-                puncher = 0;
+                stopPuncher();
                 puncherRunning = false;
             }
         }
@@ -24,7 +24,7 @@ void runPuncherToggle() {
 
 void startPuncher() {
     puncherRunning = true;
-    puncher = 127;
+    puncher = 100;
 }
 
 void stopPuncher() {
